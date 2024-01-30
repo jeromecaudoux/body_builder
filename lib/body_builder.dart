c747ce0f485aed911cb8a67bda17a66b0a259930
Jerome Caudoux <jerome@skyhook.fr>
2024-01-30 15:44:57 +0100

    [Import/Export] Fix import and export

--- lib/body_builder.dart
+++ lib/body_builder.dart
@@ -6,3 +6,5 @@ export 'src/body_state.dart';
 export 'src/state_provider.dart';
 export 'src/typedefs_child_body_builder.dart';
 export 'src/load_more.dart';
+export 'src/paginated_response.dart';
+export 'src/paginated_body_builder.dart';
